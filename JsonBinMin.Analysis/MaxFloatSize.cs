73566e82387c60f386dede761172616c0631ed62
Splamy <splamyn@gmail.com>
2026-05-17 22:40:54 +0200

    cleanup

--- JsonBinMin.Analysis/MaxFloatSize.cs
+++ JsonBinMin.Analysis/MaxFloatSize.cs
@@ -36,15 +36,26 @@ public static class MaxFloatSize
 	
 	public static int GetMaxDoubleSize()
 	{
-		var max = Enumerable.Range(0, int.MaxValue)
+		var max = GetInterestingBits()
 			.AsParallel()
 			.Select(raw => {
-				var rawUInt = (ulong)raw;
+				var rawUInt = raw;
 				var half = Unsafe.As<ulong, double>(ref rawUInt);
 				var strLen = half.ToString(Constants.RoundtripDoubleFormat, CultureInfo.InvariantCulture).Length;
 				return strLen;
 			})
 			.Max();
 		return max + 1; // 1 for the sign bit
+	}
+
+	private static IEnumerable<ulong> GetInterestingBits()
+	{
+		yield return 0; // zero
+		yield return 1; // smallest denormalized number
+		yield return 0x0000000000000001; // smallest denormalized number
+		yield return 0x0000000000000002; // second smallest denormalized number
+		yield return 0x0000000000000003; // third smallest denormalized number
+		yield return 0x7FF0000000000000; // infinity
+		yield return 0x7FF8000000000000; // NaN
 	}
 }
